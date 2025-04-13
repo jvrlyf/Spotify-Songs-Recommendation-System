c2e1921598b0bec49efc6e026ad71b1efb1f6aa7
MuKu <jvr@MuKus-MacBook-Air.local>
2025-04-13 21:52:15 +0530

    Project name update

---
+++ config/config.py
@@ -0,0 +1,4 @@
+# Configuration file for Spotify API credential
+SPOTIFY_CLIENT_ID = 'YOUR_SPOTIFY_CLIENT_ID'
+SPOTIFY_CLIENT_SECRET = 'YOUR_SECRET_ID'
+PLAYLIST_LINK = "https://open.spotify.com/playlist/2wQaxhZC3HXtmWd2yjqJG7?si=bc4f208ae2a14ab2"
